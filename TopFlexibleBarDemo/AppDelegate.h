3c7abd554a5dfbccc5da3315afae545196e02657
Robert Yu <robertyu@Roberts-iMac.local>
2017-09-29 14:57:26 +0800

    Initial Commit

---
+++ TopFlexibleBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopFlexibleBarDemo
+//
+//  Created by Robert Yu on 29/09/2017.
+//  Copyright © 2017 Robert Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TopFlexibleBarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TopFlexibleBarDemo
+//
+//  Created by Robert Yu on 29/09/2017.
+//  Copyright © 2017 Robert Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
